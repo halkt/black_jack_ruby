8e29ba49b19d96cb165b00d6373dedf5296a14fb
“halmuda” <halmuda6231@gmail.com>
2019-09-28 16:35:33 +0900

    add: エースの処理を追加

--- blackjack.rb
+++ blackjack.rb
@@ -24,13 +24,13 @@ class Card
   def display_number
     case @number
     when 1 then
-      return "A"
+      return "A(エース)"
     when 11 then
-      return "J"
+      return "J(ジャック))"
     when 12 then
-      return "Q"
+      return "Q(クイーン)"
     when 13 then
-      return "K"
+      return "K(キング)"
     else
       return @number.to_s
     end
@@ -68,27 +68,48 @@ end
 
 # ユーザークラス
 class User
+  attr_accessor :name
   attr_accessor :list
   attr_accessor :point
-  attr_accessor :burst_flg
 
-  def initialize
+  def initialize(name)
+    @name = name
     @list = []          # 手札
     @point = 0          # 合計点数
-    @burst_flg = false  # バーストフラグ
   end
 
   def draw_card(deck)
-    @list.push(deck.draw)
-    @point += @list.last.calc_number
-    @burst_flg = check_burst(@point)
+    @list.push(deck.draw)   # カードを引いて手札に追加する
+    calc_point              # 手札の合計を計算する
+    check_burst   # バーストしていないか確認する
   end
 
-  def check_burst(point)
-    if point > 21
-      return true
-    else
-      return false
+  def calc_point
+    @point = 0
+
+    # 手札の1以外の合計点を算出する
+    @list.each do |card|
+      if card.number != 1
+        @point += card.calc_number
+      end
+    end 
+
+    # 手札にある1の計算を行う
+    @list.each do |card|
+      if card.calc_number == 1
+        if @point > 10
+          @point += 1
+        else
+          @point += 11
+        end
+      end
+    end
+  end
+
+  def check_burst
+    if @point > 21
+      puts "合計『#{@point}』でバーストです。#{@name}の負けです。"
+      exit
     end
   end
 
@@ -100,18 +121,14 @@ class Player < User
     puts "カードを引きますか？引く場合は『YES』、勝負する場合は『GO(YES以外)』を入力してください！"
     response = gets.downcase.chomp
     while response == "yes"
-      draw_card(deck)
-      self.list.last.show
-      if @point > 21
-        puts "合計『#{@point}』でバーストです。あなたの負けです。"
-        exit
-      else
-        puts "合計は『#{@point}』です。もう一度引きますか？"
-        response = gets.downcase.chomp
-      end
+      draw_card(deck)         # カードを引く
+      self.list.last.show     # 結果を表示する
+      puts "合計は『#{@point}』です。もう一度引きますか？"
+      response = gets.downcase.chomp
     end
   end
 end
+
 # ディーラークラス
 class Dealer < User
   def repeat_17_points_exceeded(deck)
@@ -123,12 +140,27 @@ end
 
 # ---------------------------ここまでクラス
 
+# 勝負判定用のメソッド
+def goBattle(sum_player_card, sum_dealer_card)
+	puts "あなたのカードの合計は『#{sum_player_card}』!"
+  puts "ディーラーのカードの合計は『#{sum_dealer_card}』!"
+  puts
+	if sum_player_card < sum_dealer_card
+		puts "あなたの敗北です。You Lose..."
+	elsif sum_player_card > sum_dealer_card
+		puts "あなたの勝利です。You Wins!"
+  else
+		puts "引き分けです。draw game..."
+	end
+
+end
+
 # デッキを用意する
 deck = Deck.new
 
 # プレイヤーを用意する
-player = Player.new
-dealer = Dealer.new
+player = Player.new("プレイヤー")
+dealer = Dealer.new("ディーラ(CPU)")
 
 # プレイヤーとディーラーにカードを配る
 2.times do
@@ -152,28 +184,6 @@ player.add_card(deck)
 
 # ディーラーは17以上が出るまで引き続ける
 dealer.repeat_17_points_exceeded(deck)
-
-
-# ---------------------------ここからは配列バージョン
-
-
-
-
-# バトル
-def goBattle(sum_player_card, sum_dealer_card)
-	puts "あなたのカードの合計は『#{sum_player_card}』!"
-	puts "ディーラーのカードの合計は『#{sum_dealer_card}』!"
-	if sum_player_card < sum_dealer_card
-		puts "お前の負けだ！"
-	elsif sum_player_card > sum_dealer_card
-		puts "お前の...勝ちだ!!!!!!"
-	else
-		puts "ドローッッ!"
-	end
-
-end
-
-
 
 # 勝負!!
 goBattle(player.point, dealer.point)
